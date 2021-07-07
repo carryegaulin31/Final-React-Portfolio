bccffa3357b6c659168c481dcf36ecf8e4168cbc
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-07-07 09:53:09 -0400

    cleaned out and commited src file

---
+++ my-portfolio/src/data.js
@@ -0,0 +1,67 @@
+export const projects = [
+  {
+    title: "Great Novels API",
+    subtitle: "Express, Node, MySQL",
+    description:
+      "API of select list of the all time greats. Accesibility to novels, authors or genres and any combination in between. Fuzzy search available for titles and authors.",
+    image: "./novels.gif",
+    link: "http://novelsapi-env.eba-uftmpdwp.us-east-2.elasticbeanstalk.com/",
+  },
+  {
+    title: "DeisHacks",
+    subtitle: "Built with React Native, Express, MySQL and Nodemon",
+    description:
+      "Collaborated with a team of six to build a mobile application for the Boston Police Teen Academy to keep teens and mentors connected in a safe space during and after the programas well as giving access to one-on-one support, community mental and physical health and events. Worked closely with client/founder to diagnose and prioritize features and services that should be included in the final product. Application built from idea to MVP in 48 hours. My primary contribution was backend. Final product exceeded the expectations of the client as well as won a top prize in the hack-a-thon.",
+    image: "./BTPA.gif",
+    link: "https://youtu.be/8tXSfM07E7E",
+  },
+  {
+    title: "Once-and-Floral UI",
+    subtitle: "React.js, Express, MySql, Webpack, Node.js",
+    description:
+      "Under construction, this UI currently has a functioning API and searchable UI.  Upon completion it will have a user log in, links to flower images for comparing and access to multiple users.  This will be a tool for brides, their family and friends as well as wedding planners to be to prepare for the daunting task of flower selection, especially for destination weddings.  You can choose your arrangements all from the comfort of home if you see fit and simply share your link with anyone that needs to know or weigh in.",
+    image: "./project-4.gif",
+    link: "",
+  },
+  {
+    title: "Memory Game",
+    subtitle: "React, Next.js, React Spring",
+    description:
+      "Another project I am excited to get started creating a memory game from scratch and learn more about Next.js",
+    image: "./project-3.gif",
+    link: "",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Carrye's greatest virtues undoubtedly are her team leadership and morale building skills in addition to her ability to maintain utmost professional behavior under high pressure and stressful situations. Furthermore, Carrye is also a fast and adaptable learner, takes initiative with very little instruction, dependable, honest, and an all around exceptional person.  She was an absolute joy to work with. She would be a tremendous asset to any company and has my highest recommendation. ",
+    image: "./TJ1.jpg",
+    name: "T.J. Naylor",
+    company: "General Manager, Hog Heaven, Islamorada, FL",
+  },
+  {
+    quote:
+      "I believe that having a background in hospitality transcends into many other fields as a major focal point in interacting with guest/client satisfaction as well as working and contributing to a team, which Carrye excels at. She always fulfilled her responsibilities and met or beat any expectations. Carrye is an asset to any company as she is great under pressure, a fast learner, hard working, takes direction well, takes initiative,is dependable, honest and an all around good person.",
+    image: "./jason1.jpg",
+    name: "Jason White",
+    company: "Director of Operations, Hill Restaurant Group, Washington DC",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Node.js",
+  "MySQL",
+  "UI Design",
+  "HTML5",
+  "CSS3",
+  "Express",
+  "REST API",
+  "Postman",
+  "Lucid Charts",
+  "TDD testing/Mocha/Chai/Sinon"
+  
+];
