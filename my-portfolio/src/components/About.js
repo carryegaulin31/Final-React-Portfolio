4d51693c8d324d333edc3399a1e3552f7e90f338
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-11-20 12:35:44 -0500

    minor profile edits

--- my-portfolio/src/components/About.js
+++ my-portfolio/src/components/About.js
@@ -6,7 +6,7 @@ export default function About() {
           <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
               <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-              Hello and welcome! I'm Carrye.
+              Hello and welcome to my profile! My name is Carrye Gaulin.
                 <br className="hidden lg:inline-block" /> I love sales, meeting new people, learning new technology, and building web apps. I am looking for a Sales/Solutions/Presales Engineering role. I am open to relocation for the right team. 
               </h1>
               <p className="mb-8 leading-relaxed">
