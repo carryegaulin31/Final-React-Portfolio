733c9e3e3aaaacee0d350fc68dee3d6aa3d90a45
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-12-09 07:13:32 -0500

    changed side project

--- my-portfolio/src/data.js
+++ my-portfolio/src/data.js
@@ -9,7 +9,7 @@ export const projects = [
   },
   {
     title: "DeisHacks",
-    subtitle: "Built with React Native, Express, MySQL and Nodemon",
+    subtitle: "Built with React Native, Express, MySQL and Nodemon. Click to view presentation",
     description:
       "Collaborated with a team of six to build a mobile application for the Boston Police Teen Academy to keep teens and mentors connected in a safe space during and after the programas well as giving access to one-on-one support, community mental and physical health and events. Worked closely with client/founder to diagnose and prioritize features and services that should be included in the final product. Application built from idea to MVP in 48 hours. My primary contribution was backend. Final product exceeded the expectations of the client as well as won a top prize in the hack-a-thon.",
     image: "./BTPA.gif",
